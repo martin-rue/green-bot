81f3bf2d2b6ceca861ff1a799ef674a2e45e9c5b
Andreea-Daniela <A.Baciu@shell.com>
2023-08-31 16:10:03 +0100

    first round

--- bot.js
+++ bot.js
@@ -20,10 +20,53 @@ export const start = ({ radar }) => {
 
 export const turn = ({ minesRemaining, opponentUsedRadar }) => {
   state.turn += 1;
-  state.minesRemaining = minesRemaining;
-  state.opponentUsedRadar = opponentUsedRadar;
+  // state.minesRemaining = minesRemaining;
+  // state.opponentUsedRadar = opponentUsedRadar;
 
-  return move(state.position.x, state.position.y);
+  // return move(state.position.x, state.position.y);
+
+  state.position.x = Math.floor(Math.random()*10);
+  state.position.y = Math.floor(Math.random()*10);
+
+
+  if(turn % 2 == 1 ){
+    if(opponentUsedRadar){
+      if( mines.includes({position})){
+        if(x=9){
+          x = x -2;
+        } else if(x=0){
+          x = x + 3;
+        } else {
+          x = x + 1;
+        }
+
+        if(y=9){
+          y = y  -2;
+        } else if(y =0){
+          y = y + 3;
+        } else {
+          y = y + 1;
+        }
+
+
+      } else {
+        return move(state.position.x, state.position.y);
+      }
+    } else {
+      if(state.mineRemaining>0){
+        return layMine(state.position.x, state.position.y);
+      } else {
+
+      }
+    }
+  } else {
+    if(opponentUsedRadar){
+      return move(state.position.x, state.position.y);
+    } else {
+      return runRadar();
+    }
+  }
+
 };
 
 export const handleRadar = ({ radar }) => {
